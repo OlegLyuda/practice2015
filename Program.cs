06d67d261f68fbbdd4c3ebce0a9017c7aa9d7f8e
unknown <C:\Users\user\AppData\Roaming\The Bat!>
2015-07-02 20:56:14 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public double function(double x)
+        {
+            double f;
+            f = x * x - 2;
+            return f;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
